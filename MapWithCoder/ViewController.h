97d98a9eabfd2913e07f965ffc141b6b78656990
LengQiuHui <1205632644@qq.com>
2016-06-07 09:47:18 +0800

    Initial Commit

---
+++ MapWithCoder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapWithCoder
+//
+//  Created by ma c on 16/6/7.
+//  Copyright © 2016年 gdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MapWithCoder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MapWithCoder
+//
+//  Created by ma c on 16/6/7.
+//  Copyright © 2016年 gdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
